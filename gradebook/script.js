f544e0060accc3c94beb19bce85d0f863649caeb
rafaelamachadocamara <rafaelamachado.camara@yahoo.com.br>
2025-02-20 01:15:18 -0300

    criando sistema para chechar notas escolares

---
+++ gradebook/script.js
@@ -0,0 +1,39 @@
+function getAverage(scores) {
+    let sum = 0;
+
+    for (const score of scores) {
+        sum += score;
+    }
+
+    return sum / scores.length;
+}
+
+function getGrade(score) {
+    if (score === 100) {
+        return "A++";
+    } else if (score >= 90) {
+        return "A";
+    } else if (score >= 80) {
+        return "B";
+    } else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
+
+function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+}
+
+function studentMsg(totalScores, studentScore) {
+    const classAverage = getAverage(totalScores);
+    const studentGrade = getGrade(studentScore);
+
+    const resultMsg = studentGrade === "F" ? "You failed the course." : "You passed the course."
+
+    return `Class average: ${classAverage}. Your grade: ${studentGrade}. ${resultMsg}`;
+
+}
